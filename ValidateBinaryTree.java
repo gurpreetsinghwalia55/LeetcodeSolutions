939a19b38b373c611816890cd4204c7ffbfd46b9
gurpreetsinghwalia55 <32846169+gurpreetsinghwalia55@users.noreply.github.com>
2021-01-01 21:33:56 +0530

    Create ValidateBinaryTree.java

---
+++ ValidateBinaryTree.java
@@ -0,0 +1,14 @@
+class Solution {
+    private Integer prev;
+    public boolean inorder(TreeNode root){
+        if(root == null) return true;
+        if(!inorder(root.left)) return false;
+        if(prev != null && root.val <= prev) return false;
+        prev = root.val;
+        return inorder(root.right);
+    }
+    public boolean isValidBST(TreeNode root) {
+        prev = null;
+        return inorder(root);
+    }
+}
